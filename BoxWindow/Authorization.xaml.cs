87898e341ac50024041115e9f9a5081fc25f0287
K5092022KST@outlook.com <K5092022KST@outlook.com>
2023-01-12 14:24:58 +0300

    12/01

--- BoxWindow/Authorization.xaml.cs
+++ BoxWindow/Authorization.xaml.cs
@@ -30,13 +30,13 @@ namespace RecordPeriphelTechniс.BoxWindow
             InitializeComponent();
         }
 
-       public void CheckerText()
+        public void CheckerText()
         {
             SimpleComand.CheckTextBox(TextBoxLogin);
             SimpleComand.CheckPassBox(PassBox);
         } //Проверка пустых строк(подсветка)
 
-        public void AuthorizationUser() 
+        public void AuthorizationUser()
         {
             try
             {
@@ -49,51 +49,64 @@ namespace RecordPeriphelTechniс.BoxWindow
                         MessageBox.Show("Заполните обязательные поля ", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                     }
                     else
-                    {                       
+                    {
+
                         connection.Open();
                         var Pass = SimpleComand.GetHash(PassBox.Password);
                         string LoginLower = TextBoxLogin.Text.ToLower();
-                        string query = $@"SELECT  COUNT(1) FROM Users WHERE Login='{LoginLower}' AND Password = @Password and IDStatus != 3";
+                        //string query = $@"SELECT  COUNT(1) FROM Users WHERE Login='{LoginLower}' AND Password = @Password";
+                        string query = $@"SELECT  COUNT(1) FROM Users WHERE Login='{LoginLower}'";
                         SQLiteCommand cmd = new SQLiteCommand(query, connection);
-                        cmd.Parameters.AddWithValue("@Password", Pass);
-                        int UsersFound = Convert.ToInt32(cmd.ExecuteScalar());
-                        if (UsersFound == 1)
+                        // cmd.Parameters.AddWithValue("@Password", Pass);
+                        int UsersSearch = Convert.ToInt32(cmd.ExecuteScalar());
+                        if (UsersSearch == 1)
                         {
-                            query = $@"SELECT ID,IDAllowance FROM Users WHERE Login= '{LoginLower}'";
-                            Saver.LoginUser = LoginLower;
-                            SQLiteDataReader dr = null;
-                            SQLiteCommand cmd1 = new SQLiteCommand(query, connection);
-                            int IDAllowance = 0;
-                            dr = cmd1.ExecuteReader();
-                            while (dr.Read())
+                            query = $@"SELECT  COUNT(1) FROM Users WHERE Login='{LoginLower}' AND Password = @Password and IDStatus != 3";
+                            cmd = new SQLiteCommand(query, connection);
+                            cmd.Parameters.AddWithValue("@Password", Pass);
+                            int UsersFound = Convert.ToInt32(cmd.ExecuteScalar());
+                            if (UsersFound == 1)
                             {
-                                Saver.IDUser = dr["ID"].ToString();
-                                IDAllowance  = Convert.ToInt32(dr["IDAllowance"].ToString());
-                                //  Saver.IDAcc = countID;
-                            }
-                            if (IDAllowance == 1)
-                            {
-                                MessageBox.Show("Добро пожаловать!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
-                                AdminPanel admpnl = new AdminPanel();
-                                this.Close();
-                                admpnl.ShowDialog();
+                                query = $@"SELECT ID,IDAllowance FROM Users WHERE Login= '{LoginLower}'";
+                                Saver.LoginUser = LoginLower;
+                                SQLiteDataReader dr = null;
+                                SQLiteCommand cmd1 = new SQLiteCommand(query, connection);
+                                int IDAllowance = 0;
+                                dr = cmd1.ExecuteReader();
+                                while (dr.Read())
+                                {
+                                    Saver.IDUser = dr["ID"].ToString();
+                                    IDAllowance = Convert.ToInt32(dr["IDAllowance"].ToString());
+                                    //  Saver.IDAcc = countID;
+                                }
+                                if (IDAllowance == 1)
+                                {
+                                    MessageBox.Show("Добро пожаловать!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                                    AdminPanel admpnl = new AdminPanel();
+                                    this.Close();
+                                    admpnl.ShowDialog();
+                                    connection.Close();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Добро пожаловать!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                                    MenuInformation menuinfor = new MenuInformation();
+                                    this.Close();
+                                    menuinfor.ShowDialog();
+                                    connection.Close();
+                                }
+
                                 connection.Close();
+                                //MessageBox.Show("Добро пожаловать! " + $@"{TextBoxLogin.Text}");
                             }
                             else
                             {
-                                MessageBox.Show("Добро пожаловать!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
-                                MenuInformation menuinfor = new MenuInformation();
-                                this.Close();
-                                menuinfor.ShowDialog();
-                                connection.Close();
+                                NotInvalidPass();
                             }
-
-                            connection.Close();
-                            //MessageBox.Show("Добро пожаловать! " + $@"{TextBoxLogin.Text}");
                         }
                         else
                         {
-                            NotInvalidPass();
+                            MessageBox.Show("Неверный пароль или логин", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                         }
                     }
                 }
@@ -114,10 +127,14 @@ namespace RecordPeriphelTechniс.BoxWindow
                     connection.Open();
                     var Pass = SimpleComand.GetHash(PassBox.Password);
                     string LoginLower = TextBoxLogin.Text.ToLower();
-                    string  query = $@"SELECT  COUNT(1) FROM Users WHERE Login = '{LoginLower}' and Password != @Password";
+                    string query = $@"SELECT  COUNT(1) FROM Users WHERE Login = '{LoginLower}' and Password != @Password";
                     SQLiteCommand cmd = new SQLiteCommand(query, connection);
                     cmd.Parameters.AddWithValue("@Password", Pass);
                     int proverkaInvalidPass = Convert.ToInt32(cmd.ExecuteScalar());
+                    query = $@"SELECT  COUNT(1) FROM Users WHERE Login='{LoginLower}' AND Password = @Password and IDStatus = 3";
+                    cmd = new SQLiteCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@Password", Pass);
+                    int UsersUnBlock = Convert.ToInt32(cmd.ExecuteScalar());
                     if (proverkaInvalidPass == 1)
                     {
                         query = $@"SELECT IDAllowance FROM Users WHERE Login= '{LoginLower}'";
@@ -155,49 +172,124 @@ namespace RecordPeriphelTechniс.BoxWindow
 
                             int kolint = Convert.ToInt32(AttemptNumber);
                             kolint++;
-                            string timeban = "0:01";
-                            TimeSpan s1 = TimeSpan.Parse("0:01");
+                            //string timeban = "0:02";
+                            TimeSpan s1 = TimeSpan.Parse("0:02");
                             TimeSpan s3 = s1 + s2;
                             string times3 = s3.ToString("hh':'mm");
                             query = $@"UPDATE Proverka SET AttemptNumber='{kolint}',TimeBegin='{dateOpen}',TimeEnd ='{times3}' WHERE ID ='{IDProverka}';";
                             cmd = new SQLiteCommand(query, connection);
                             cmd.ExecuteReader();
+                            MessageBox.Show("Неверный пароль или логин", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
 
                         }
                         else if (Convert.ToInt32(AttemptNumber) == 3)
                         {
                             if (Convert.ToInt32(AttemptNumber) == 3 && (TimeSpan.Parse(dateOpen) < TimeSpan.Parse(dateban)))
                             {
-                                MessageBox.Show("попробуйте позже");//попробуйте позже
                                 query = $@"UPDATE Users SET IDStatus='{3}' WHERE Login='{LoginLower}';";
                                 cmd = new SQLiteCommand(query, connection);
                                 cmd.ExecuteReader();
+                                MessageBox.Show("Ваша учетная запись временно заблокированна,попробуйте позже", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                             }
-                            else if (Convert.ToInt32(AttemptNumber) == 3 && (TimeSpan.Parse(dateOpen) > TimeSpan.Parse(dateban)))
+                            else if (Convert.ToInt32(AttemptNumber) == 3 && (TimeSpan.Parse(dateOpen) >= TimeSpan.Parse(dateban)))
                             {
-                                query = $@"UPDATE Proverka SET AttemptNumber = '{AttemptNumber}' WHERE ID ='{IDProverka}';";
+                                query = $@"UPDATE Proverka SET AttemptNumber = '{0}',TimeBegin = '00:00',TimeEnd = '00:00' WHERE ID ='{IDProverka}';";
                                 cmd = new SQLiteCommand(query, connection);
                                 cmd.ExecuteReader();
                                 query = $@"UPDATE Users SET IDStatus='{1}' WHERE Login='{LoginLower}';";
                                 cmd = new SQLiteCommand(query, connection);
                                 cmd.ExecuteReader();
+                                MessageBox.Show("Неверный пароль или логин", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                             }
                             else
                             {
-                               // MessageBox.Show("Ban");
-                               
+
+
                             }
                         }
                     }
+                    if (UsersUnBlock == 1)// снятие времменгой блокировки при правельном пароле,при котором время блокировки еще не прошло
+                    {
+                        query = $@"SELECT IDAllowance,IDProverka FROM Users WHERE Login= '{LoginLower}'";
+                        Saver.LoginUser = LoginLower;
+                        SQLiteDataReader dr = null;
+                        SQLiteCommand cmd1 = new SQLiteCommand(query, connection);
+                        int IDAllowance = 0;
+                        int IDProverka = 0;
+                        dr = cmd1.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            // Saver.IDUser = dr["ID"].ToString();
+                            IDAllowance = Convert.ToInt32(dr["IDAllowance"].ToString());
+                            IDProverka = Convert.ToInt32(dr["IDAllowance"].ToString());
+                            //  Saver.IDAcc = countID;
+                        }
+                        query = $@"SELECT AttemptNumber,TimeEnd,TimeBegin FROM Proverka WHERE ID = '{IDProverka}';";
+                        cmd = new SQLiteCommand(query, connection);
+                        string dateOpen = DateTime.Now.ToString("t");
+                        TimeSpan s2 = TimeSpan.Parse(dateOpen);
+                        dr = null;
+                        string AttemptNumber = "";
+                        string dateban = "00:00";
+                        string dateBegin = "00:00";
+                        dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            AttemptNumber = dr["AttemptNumber"].ToString();
+                            dateban = dr["TimeEnd"].ToString();
+                            dateBegin = dr["TimeBegin"].ToString();
+
+                        }
+                        if (Convert.ToInt32(AttemptNumber) == 3 && (TimeSpan.Parse(dateOpen) > TimeSpan.Parse(dateban)))
+                        {
+                            query = $@"UPDATE Proverka SET AttemptNumber = '{0}',TimeBegin = '00:00',TimeEnd = '00:00' WHERE ID ='{IDProverka}';";
+                            cmd = new SQLiteCommand(query, connection);
+                            cmd.ExecuteReader();
+                            query = $@"UPDATE Users SET IDStatus='{1}' WHERE Login='{LoginLower}';";
+                            cmd = new SQLiteCommand(query, connection);
+                            cmd.ExecuteReader();
+                            query = $@"SELECT ID FROM Users WHERE Login= '{LoginLower}'";
+                            Saver.LoginUser = LoginLower;
+                            dr = null;
+                            cmd1 = new SQLiteCommand(query, connection);
+                            dr = cmd1.ExecuteReader();
+                            while (dr.Read())
+                            {
+                                Saver.IDUser = dr["ID"].ToString();
+
+                            }
+                            if (IDAllowance == 1)
+                            {
+                                MessageBox.Show("Добро пожаловать!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                                AdminPanel admpnl = new AdminPanel();
+                                this.Close();
+                                admpnl.ShowDialog();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Добро пожаловать!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                                MenuInformation menuinfor = new MenuInformation();
+                                this.Close();
+                                menuinfor.ShowDialog();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ваша учетная запись временно заблокированна,попробуйте позже", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+
+
+                    }
+                    connection.Close();
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Ошибка" + ex);
             }
-        }
+        } //Если пароль не правельный
 
-            private void TextBoxLogin_TextChanged(object sender, TextChangedEventArgs e)
+        private void TextBoxLogin_TextChanged(object sender, TextChangedEventArgs e)
         {
             SimpleComand.CheckTextBox(TextBoxLogin);
         }
@@ -208,7 +300,7 @@ namespace RecordPeriphelTechniс.BoxWindow
         }
 
         private void BtnAvtoriz_Click(object sender, RoutedEventArgs e)
-        {           
+        {
             AuthorizationUser();
         }
     }
