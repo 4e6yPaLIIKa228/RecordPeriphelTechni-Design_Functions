9aa5007f2aed5d90811588eab858bdaae858c732
K5092022KST@outlook.com <K5092022KST@outlook.com>
2023-02-03 12:31:08 +0300

    03.02

--- BoxWindow/MasterPanel.xaml.cs
+++ BoxWindow/MasterPanel.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using RecordPeriphelTechniс.BoxWindow;
+using RecordPeriphelTechniс.Connection;
+using RecordPeriphelTechniс.Windows;
 
 namespace RecordPeriphelTechniс.BoxWindow
 {
@@ -22,6 +27,35 @@ namespace RecordPeriphelTechniс.BoxWindow
         public MasterPanel()
         {
             InitializeComponent();
+            LoadDB_Application();
         }
+
+        public void LoadDB_Application()
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(DBConnection.myConn))
+                {
+                    connection.Open();
+                    string query = $@"SELECT RepairDevice.ID, RepairDevice.IDMaster, RepairDevice.IDDevice, StatusApplications.NameStatus as Status , Users.Surname as Master,
+ RepairDevice.DateAppeals, RepairDevice.Comment  FROM RepairDevice
+ JOIN StatusApplications on RepairDevice.IDStatus  =  StatusApplications.ID
+ JOIN Users on RepairDevice.IDMaster = Users.ID";
+                    SQLiteCommand cmd = new SQLiteCommand(query, connection);
+                    DataTable DT = new DataTable("RepairDevice");
+                    SQLiteDataAdapter SDA = new SQLiteDataAdapter(cmd);
+                    SDA.Fill(DT);
+                    ListApplications.ItemsSource = DT.DefaultView;
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
     }
 }
