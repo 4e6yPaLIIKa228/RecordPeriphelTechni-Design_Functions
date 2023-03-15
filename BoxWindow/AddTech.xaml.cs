8ef2ef534d39195a4bbd2c2e03ca241d2b10a57d
4e6yPaLIIKa228 <alexander_pushkar@mail.ru>
2023-03-15 19:26:32 +0300

    15/03/23
    15/03/23

--- BoxWindow/AddTech.xaml.cs
+++ BoxWindow/AddTech.xaml.cs
@@ -994,10 +994,20 @@ namespace RecordPeriphelTechniс.BoxWindow
             }
         }
 
+        public void TextValidationTextBox(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) e.Handled = true;
+        }
+
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            Regex regex = new Regex("[^0-9,$]");
+            Regex regex = new Regex("[^0-9$]");
             e.Handled = regex.IsMatch(e.Text); 
+        }
+        private void NumberValidationTextBoxZapud(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9,$]");
+            e.Handled = regex.IsMatch(e.Text);
         }
         private void TextValidationTextBox(object sender, TextCompositionEventArgs e)
         {
